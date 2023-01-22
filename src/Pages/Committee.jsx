f89aacd366348abe500d6a1d0b573012f83a4c4f
Parth <parth.sce21@sot.pdpu.ac.in>
2023-01-23 00:14:01 +0530

    Added committee Page

--- src/Pages/Committee.jsx
+++ src/Pages/Committee.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
+import ProfileCard from '../components/Committee/ProfileCard'
+import CommitteeMain from '../components/Committee/CommitteeMain'
 const Committee = () => {
-  return (
-    <div>Committee</div>
-  )
-}
 
-export default Committee
+  return (
+    <div className='bg-black font-Lato '>
+    <div className='h-[80vh] flex bg-black flex-col'>
+      <Navbar/>
+      <CommitteeMain/>
+      <h1 className=' pt-28 uppercase text-6xl bg-black text-primary font-light font-Bebas text-center'>Advisors</h1>
+      <ProfileCard/>
+    </div>
+    </div>
+  )
+} 
+ 
+export default Committee 
