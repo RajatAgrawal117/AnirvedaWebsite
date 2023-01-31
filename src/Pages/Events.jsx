d0182abd4f8df087acb8bab13c264463d8abb339
Preet Sojitra <preet.dev373@gmail.com>
2023-01-31 23:33:55 +0530

    add: contact us footer in events page

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import EventsPage from "../components/Events/EventsPage"
 import Navbar from "../components/Navbar"
+import ContactUs from "../components/Home/ContactUs"
 
 const Events = () => {
   return (
     <div className="bg-black font-Lato">
       <Navbar />
-      <EventsPage/>
+      <EventsPage />
+      <ContactUs />
     </div>
   )
 }
