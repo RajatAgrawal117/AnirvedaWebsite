874b097a28eba002ef2c9c40f2d1d878b213234c
Preet Sojitra <preet.dev373@gmail.com>
2023-02-01 14:03:16 +0530

    fix: change event name

--- src/data/comingEvents.js
+++ src/data/comingEvents.js
@@ -1,30 +1,30 @@
 export const comingEvents = [
   {
     id: 1,
-    title: "Chichen Itza",
+    title: "Ambush Marketing",
     date: "4th Feb 2023",
     description:
       "Anirveda, the techno-economics of PDEU, organizes a fun event 'Ambush Marketing' where rivaling companies compete against other by outdoing and directly attacking the rival’s marketing strategy to emerge as the best. The objective of the event is to engage participants in ‘ambush marketing’ and understand its impact on companies. In the event, students in a group of 2-4 advertise themselves by chaffing about their rival companies over their loopholes and hence showcasing how they are better. The judgement is based on two criteria: domination and creativity.",
     registrationLink: "https://forms.gle/Jx6nQ9bo1mvPt5KZ7",
-    img: "images/ambush.webp"
+    img: "images/ambush.webp",
   },
   {
     id: 2,
-    title: "Colosseum Roma",
+    title: "Globalopoly",
     date: "4th Feb 2023",
     description:
       "Anirveda, the TechnoEconomics Club of PDEU, organizes a riveting auction-based event 'Globalopoly' for Tesseract. In the Monopoly-inspired game, the participating teams with 2-4 members each are provided with a country profile. The game proceeds with bidding for 14 resources at base price for single unit. A crisis round is declared before the trading round. After trading, internal trading will also be allowed. Finally the team which will be able to justify its resources and trading will be declared winner..",
     registrationLink: "https://forms.gle/Stx3fN9UZRSeZoDX9",
-    img: "images/global.webp"
+    img: "images/global.webp",
   },
   {
     id: 3,
-    title: " Great Pyramid of Giza",
+    title: " ISS Odyssey",
     date: "4th Feb 2023",
     description:
-      "This is dummy text suggested by Github Copilot and it needs to be changed. Anirveda, the techno-economics club of PDEU, organizes a fun event 'ISS Odyssey' for Tesseract. In the event, participants are provided with a set of resources and a mission to be accomplished. The participants are required to design a space station with the given resources and justify their design. The team which will be able to justify its design will be declared winner.",
+      "It's The Game of Life but in SPACE! Where 4 players will go through simulations of a person's travels through their life, from early adulthood to retirement having gained necessary jobs along the way. They'll start from Earth with a filled fuel tank and wander around the cosmos, landing at different blocks where they might get benefits or even lose what they have! The player who becomes most successful in the wins!",
     registrationLink: "https://forms.gle/pAayGD9mogKRPtFk9",
-    img: "images/iss.webp"
+    img: "images/iss.webp",
   },
   // {
   //   id: 4,
