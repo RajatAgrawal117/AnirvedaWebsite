14951fafc05908eae16f64f08a04e03bdc2543ab
Rajat Agrawal <rajat.ace22@sot.pdpu.ac.in>
2025-08-29 13:22:35 +0530

    position changes

--- src/data/committee/advisors.js
+++ src/data/committee/advisors.js
@@ -3,28 +3,28 @@ const advisors = [
     id: 1,
     img_src: "https://res.cloudinary.com/duygdcgj3/image/upload/w_300,f_auto,q_auto/v1755525786/Aayushi_Advisor_Photo4_-_22BABBA201_AayushiT_vckp6a.heic",
     name: "Aayushi Thakkar",
-    position: "Ex-President",
+    position: "Advisor",
     linkedIn: "https://www.linkedin.com/in/aayushi-thakkar-4213842b6/",
   },
   {
     id: 2,
     img_src: "https://res.cloudinary.com/duygdcgj3/image/upload/w_300,f_auto,q_auto/v1755525791/Sumer_Advisor_Photo4_-_Sumer_Pandey_bjaine.heic",
     name: "Sumer Pandey",
-    position: "Ex-Vice President",
+    position: "Advisor",
     linkedIn: "https://www.linkedin.com/in/sumerpandey/",
   },
   {
     id: 3,
     img_src: "https://res.cloudinary.com/duygdcgj3/image/upload/w_300,f_auto,q_auto/v1755525789/IMG_3343_-_Shreya_Patel_l16f25.heic",
     name: "Shreya Patel",
-    position: "Ex-Chief Coordinator",
+    position: "Advisor",
     linkedIn: "https://www.linkedin.com/in/patelshreya7056/"
   },
   {
     id: 4,
     img_src: "https://res.cloudinary.com/duygdcgj3/image/upload/w_300,f_auto,q_auto/v1755525798/Rajat_advisor_photo3_-_Rajat_Agrawal_hrsdsj.jpg",
     name: "Rajat Agrawal",
-    position: "Ex-Treasurer",
+    position: "Advisor",
     linkedIn: "https://www.linkedin.com/in/rajat-agrawal-piyushkumar/",
   }
 ]
